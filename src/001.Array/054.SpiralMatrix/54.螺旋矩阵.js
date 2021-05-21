cd7e4cfd7de56a034ee9a2b60cac57b24da08042
hortor <>
2021-05-21 14:10:58 +0800

    54-SpiralMatrix

--- src/001.Array/054.SpiralMatrix/54.螺旋矩阵.js
+++ src/001.Array/054.SpiralMatrix/54.螺旋矩阵.js
@@ -14,55 +14,45 @@ var spiralOrder = function (matrix) {
 
 	if (!flatMatrix.length) return [];
 
-	const columnLen = matrix[0].length;
-	const rowLen = matrix.length;
 	let nextMatrix = [];
 	let count = 0;
 	let l = 0,
-		r = columnLen - 1,
+		r = matrix[0].length - 1,
 		t = 0,
-		b = rowLen - 1;
+		b = matrix.length - 1;
 
-	while (count < flatMatrix.length) {
+	while (count < flatMatrix.length && l <= r) {
 		// 从左到右
-		for (let i = l; i < columnLen; i++) {
-            console.log(matrix[t][i]);
+		for (let i = l; i <= r; i++) {
 			nextMatrix.push(matrix[t][i]);
 			count++;
 		}
 		t++;
-        console.log('-----');
+
 		// 从上到下
-		for (let i = t; i < rowLen; i++) {
-            console.log(matrix[i][r]);
+		for (let i = t; i <= b; i++) {
 			nextMatrix.push(matrix[i][r]);
 			count++;
 		}
 		r--;
-        console.log('-----');
+
 		// 从右到左
-		for (let i = r; i >= l; i--) {
-            console.log(matrix[b][i]);
-			nextMatrix.push(matrix[b][i]);
-			count++;
+		if (l <= r && t <= b) {
+			for (let i = r; i >= l; i--) {
+				nextMatrix.push(matrix[b][i]);
+				count++;
+			}
+			b--;
+
+			// 从下到上
+			for (let i = b; i >= t; i--) {
+				nextMatrix.push(matrix[i][l]);
+				count++;
+			}
+			l++;
 		}
-		b--;
-        console.log('-----');
-		// 从下到上
-		for (let i = b; i >= t; i--) {
-            console.log(matrix[i][l]);
-			nextMatrix.push(matrix[i][l]);
-			count++;
-		}
-		l++;
 	}
-	console.log(nextMatrix);
+
 	return nextMatrix;
 };
-
-spiralOrder([
-	[1, 2, 3],
-	[4, 5, 6],
-	[7, 8, 9],
-]);
 // @lc code=end

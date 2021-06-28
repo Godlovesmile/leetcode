5d980e7df3fd1a0c5d228e03f62ec702651f52b9
hortor <>
2021-06-28 20:57:09 +0800

    005.LinkedList

---
+++ src/005.LinkedList/203.RemoveLinkedListElements/203.移除链表元素.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=javascript
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    const mockHead = new ListNode(0);
+    mockHead.next = head;
+    let temp = mockHead;
+
+    while (temp.next != null) {
+        if (temp.next.val === val) {
+            temp.next = temp.next.next;
+        } else {
+            temp = temp.next;
+        }
+    }
+    console.log(mockHead);
+    return mockHead.next
+};
+// @lc code=end
+

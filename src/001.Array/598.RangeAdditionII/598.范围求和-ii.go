b76497fd4e4a150e4828a6e9912219209bb4bd68
hortor <>
2021-10-15 14:11:46 +0800

    598.RangeAdditionII-Glang

--- src/001.Array/598.RangeAdditionII/598.范围求和-ii.go
+++ src/001.Array/598.RangeAdditionII/598.范围求和-ii.go
@@ -7,16 +7,29 @@
 // @lc code=start
 func maxCount(m int, n int, ops [][]int) int {
 	newArr := make([][]int, m)
-	for i = 0; i < m; i++ {
+	for i := 0; i < m; i++ {
 		newArr[i] = make([]int, n)
 	}
 
-	for i = 0; i < len(ops); i++ {
-		for j = 0; j < len(ops[i][0]); j++ {
-			for k = 0; k < ops[i][1]; k++ {
+	for i := 0; i < len(ops); i++ {
+		for j := 0; j < ops[i][0]; j++ {
+			for k := 0; k < ops[i][1]; k++ {
+				newArr[j][k] += 1
 			}
 		}
 	}
+
+	count := 0
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if newArr[i][j] == newArr[0][0] {
+				count++
+			}
+		}
+	}
+
+	return count
 }
 
 // @lc code=end

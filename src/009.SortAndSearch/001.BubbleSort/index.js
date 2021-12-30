e5babad8c2e8b465d0d87dd183bc9bb0b48f61ca
Alice <2272674323@qq.com>
2021-12-30 23:07:37 +0800

    BubbleSort

--- src/009.SortAndSearch/001.BubbleSort/index.js
+++ src/009.SortAndSearch/001.BubbleSort/index.js
@@ -14,3 +14,20 @@ function bubbleSort(arr) {
 }
 
 console.log(bubbleSort([5, 7, 9, 2]));
+
+// 改进的冒泡排序法, 上述的排序中, 会发现后面的排序已经排好无需再进行比较
+// 进行小改进, 但是冒泡排序的复杂度还是很高(O(n^2))
+function modifiedBubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(modifiedBubbleSort([5, 7, 9, 2]));

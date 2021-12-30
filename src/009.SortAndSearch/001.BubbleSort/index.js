cff061d170249e22c9e612f81e6ff116a46cac7f
Alice <2272674323@qq.com>
2021-12-30 18:39:58 +0800

    sort

---
+++ src/009.SortAndSearch/001.BubbleSort/index.js
@@ -0,0 +1,16 @@
+// 冒泡排序
+function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([5, 7, 9, 2]));

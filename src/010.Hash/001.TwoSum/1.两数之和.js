176c14192efbb2df0e70287b5b22772ebad54824
DESKTOP-I7196D2\admin <15501251520@163.com>
2022-04-23 14:43:19 +0800

    feat: two sum by sort-point

--- src/010.Hash/001.TwoSum/1.两数之和.js
+++ src/010.Hash/001.TwoSum/1.两数之和.js
@@ -10,20 +10,41 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  let numsMap = {};
 
-  for (let i = 0; i < nums.length; i++) {
-    let other = target - nums[i];
+// map 思路
+// var twoSum = function(nums, target) {
+//   let numsMap = {};
 
-    if (other in numsMap) {
-      return [numsMap[other], i];
+//   for (let i = 0; i < nums.length; i++) {
+//     let other = target - nums[i];
+
+//     if (other in numsMap) {
+//       return [numsMap[other], i];
+//     } else {
+//       numsMap[nums[i]] = i;
+//     }
+//   }
+
+//   return [];
+// };
+
+// 双指针思路
+var twoSum = function (nums, target) {
+  const sortNums = JSON.parse(JSON.stringify(nums)).sort((a, b) => a - b);
+  let left = 0;
+  let right = sortNums.length - 1;
+
+  while (left < right) {
+    const sum = sortNums[left] + sortNums[right];
+
+    if (sum === target) {
+      return [nums.indexOf(sortNums[left]), nums.lastIndexOf(sortNums[right])];
+    } else if (sum < target) {
+      left++;
     } else {
-      numsMap[nums[i]] = i;
+      right--;
     }
   }
-
-  return [];
-};
+}
 // @lc code=end
 

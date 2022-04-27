078ad8f956d2c6b7deb831b5a97f76fac7616f62
DESKTOP-I7196D2\admin <15501251520@163.com>
2022-04-27 10:44:47 +0800

    feat: 13.roman-to-integer by go

--- src/002.String/013.RomanToInteger/13.罗马数字转整数.go
+++ src/002.String/013.RomanToInteger/13.罗马数字转整数.go
@@ -5,8 +5,19 @@
  */
 
 // @lc code=start
-func romanToInt(s string) int {
+func romanToInt(s string) (sum int) {
+	var symbolValus = map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
+	for i := 0; i < len(s); i++ {
+		cur := symbolValus[s[i]]
+		if i < len(s) - 1 && cur < symbolValus[s[i+1]] {
+			sum -= cur
+		} else {
+			sum += cur
+		}
+	}
+
+	return
 }
 // @lc code=end
 

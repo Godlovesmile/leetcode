00338a0ad1ce6621b38c6ca30d23c1d940759815
DESKTOP-I7196D2\admin <15501251520@163.com>
2022-05-05 23:03:32 +0800

    feat: 26.删除有序数组重复项 by js

--- src/001.Array/026.RemoveDuplicatesFromSortedArray/26.删除有序数组中的重复项.js
+++ src/001.Array/026.RemoveDuplicatesFromSortedArray/26.删除有序数组中的重复项.js
@@ -10,7 +10,21 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+  const len = nums.length;
 
+  if (len === 0) return 0;
+
+  let fast = 1, slow = 1;
+
+  while (fast < len) {
+    if (nums[fast] !== nums[fast-1]) {
+      nums[slow] = nums[fast];
+      ++slow;
+    }
+    ++fast;
+  }
+
+  return slow;
 };
 // @lc code=end
 

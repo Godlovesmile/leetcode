ada20f280de15b32b6978480256e943e3c5e79b2
lizhi <15501251520@163.com>
2022-06-21 16:24:00 +0800

    feat: 53.最大子数组和 by js, 动态规划思想

--- src/001.Array/053.MaximumSubarray/53.最大子数组和.js
+++ src/001.Array/053.MaximumSubarray/53.最大子数组和.js
@@ -10,7 +10,28 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
+  let max = nums[0];
+  let sum = nums[0];
+  
+  for (let i = 1; i < nums.length; i++) {
+    if (sum + nums[i] < nums[i]) {
+      sum = nums[i];
 
+      if (max < sum) {
+        max = sum;
+      }
+
+      continue;
+    }
+
+    sum = sum + nums[i];
+
+    if (max < sum) {
+      max = sum;
+    }
+  }
+  
+  return max;
 };
 // @lc code=end
 

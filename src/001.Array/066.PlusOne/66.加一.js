ef6df05d8773ac02bea585d4c1bde1f5911cc66c
lizhi <15501251520@163.com>
2022-07-26 18:29:20 +0800

    feat: 66.加一逻辑

--- src/001.Array/066.PlusOne/66.加一.js
+++ src/001.Array/066.PlusOne/66.加一.js
@@ -48,10 +48,8 @@ var plusOne = function(digits) {
     }
   }
 
-  if (digits[0] == 0) {
-    digits.unshift(1)
-  }
-
+  digits.unshift(1)
+  
   return digits
 }
 // const test = plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3])

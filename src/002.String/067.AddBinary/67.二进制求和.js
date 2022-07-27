90ab7379f00d6c81a49aa15821ed00737055c678
lizhi <15501251520@163.com>
2022-07-27 14:18:18 +0800

    feat: 067.二进制求和 by js

--- src/002.String/067.AddBinary/67.二进制求和.js
+++ src/002.String/067.AddBinary/67.二进制求和.js
@@ -11,7 +11,39 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
+  const [max, min] = a.length >= b.length ? [a, b] : [b, a]
+  let str = '', index = 0
+  let j = min.length
 
+  for (let i = max.length - 1; i >= 0; i--) {
+    let sum = Number(max[i]) + index
+
+    if (--j >= 0) {
+      sum += Number(min[j])
+    } 
+    
+    if (sum == 2) {
+      index = 1
+      str = '0' + str
+    } else if (sum == 0) {
+      index = 0
+      str = '0' + str
+    } else if (sum > 2) {
+      index = 1
+      str = '1' + str
+    } else {
+      index = 0
+      str = '1' + str
+    }
+  }
+
+  if (index) {
+    str = '1' + str
+  }
+
+  return str
 };
+// 100
+// console.log(addBinary('1111', '1111'))
 // @lc code=end
 

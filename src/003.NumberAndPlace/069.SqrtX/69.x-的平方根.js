bdaee4f3019f6184993cddf7b5c05ce3d33237a2
lizhi <15501251520@163.com>
2022-08-01 15:38:14 +0800

    feat: sqrt 69-x的平方根

--- src/003.NumberAndPlace/069.SqrtX/69.x-的平方根.js
+++ src/003.NumberAndPlace/069.SqrtX/69.x-的平方根.js
@@ -9,8 +9,24 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
 
+// 二分查找法
+var mySqrt = function(x) {
+  let l = 0, r = x, ans = -1
+
+  while (l <= r) {
+    let mid = Math.floor(l + (r - l) / 2)
+
+    if (mid * mid <= x) {
+      ans = mid
+      l = mid + 1
+    } else {
+      r = mid - 1
+    }
+  }
+
+  return ans
 };
+// console.log(mySqrt(8))
 // @lc code=end
 

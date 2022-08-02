03163c2dc016a3c1bbaa21c511c704cfa22ff521
lizhi <15501251520@163.com>
2022-08-02 14:29:36 +0800

    feat: 70.爬楼梯 by js

---
+++ src/003.NumberAndPlace/070.ClimbingStairs/70.爬楼梯.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=golang
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+func climbStairs(n int) int {
+
+}
+// @lc code=end
+
--- src/003.NumberAndPlace/070.ClimbingStairs/70.爬楼梯.js
+++ src/003.NumberAndPlace/070.ClimbingStairs/70.爬楼梯.js
@@ -9,8 +9,20 @@
  * @param {number} n
  * @return {number}
  */
+// 动态规划思想
+// 1 2 3 5 8
+// f(x) = f(x-1) + f(x-2)
 var climbStairs = function(n) {
+  let dep = []
 
+  dep[0] = 1
+  dep[1] = 2
+
+  for (let i = 2; i <= n - 1; i++) {
+    dep[i] = dep[i-1] + dep[i-2]
+  }
+
+  return dep[n-1]
 };
 // @lc code=end
 

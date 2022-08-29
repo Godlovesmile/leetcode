9fa5b67a1bca2781cea87587a53a49bba430c7ec
lizhi <15501251520@163.com>
2022-08-29 18:49:25 +0800

    feat: 相同的树深度优先搜索

--- src/008.Tree/100.same-tree/100.相同的树.js
+++ src/008.Tree/100.same-tree/100.相同的树.js
@@ -18,8 +18,48 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
+// var isSameTree = function(p, q) {
+//   const inorderTraversal = (root) => {
+//     const res = []
+//     const inorder = (root) => {
+//       if (!root) {
+//         res.push(null)
+//         return
+//       }
 
-};
+//       inorder(root.left)
+//       res.push(root.val)
+//       inorder(root.right)
+//     }
+
+//     inorder(root)
+//     return res
+//   }
+
+//   const isEqualArr = (a, b) => {
+//     if (a.length != b.length) return false
+
+//     for (let i = 0; i < a.length; i++) {
+//       if (a[i] != b[i]) return false
+//     }
+
+//     return true
+//   }
+
+//   const pArr = inorderTraversal(p)
+//   const qArr = inorderTraversal(q)
+//   console.log(pArr)
+//   console.log(qArr)
+//   return isEqualArr(pArr, qArr)
+// };
+
+// 深度优先, 递归
+var isSameTree = function(p, q) {
+  if (!p && !q) return true
+  if (!p || !q) return false
+  if (p.val != q.val) return false
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+}
 // @lc code=end
 

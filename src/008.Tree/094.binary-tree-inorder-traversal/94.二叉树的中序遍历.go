9b2a07c4456aefba7c32cdd2c24c091d21eaa644
lizhi <15501251520@163.com>
2022-08-29 18:01:50 +0800

    feat: 二叉树中序遍历 by go

---
+++ src/008.Tree/094.binary-tree-inorder-traversal/94.二叉树的中序遍历.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=golang
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 迭代思想
+func inorderTraversal(root *TreeNode) (result []int) {
+	stack := []*TreeNode{}
+
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		root = stack[len(stack)-1]
+		result = append(result, root.Val)
+		stack = stack[:len(stack)-1]
+		root = root.Right
+	}
+
+	return
+}
+
+// @lc code=end
+

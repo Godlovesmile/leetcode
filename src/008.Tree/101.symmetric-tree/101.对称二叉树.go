015c935517efed74790b4fe302513ef10b0fa696
lizhi <15501251520@163.com>
2022-09-07 18:58:56 +0800

    feat: 101.对称二叉树-递归思想 by go

---
+++ src/008.Tree/101.symmetric-tree/101.对称二叉树.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=golang
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func check(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+
+	return check(p.Left, q.Right) && check(p.Right, q.Left)
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return check(root, root)
+}
+
+// @lc code=end
+

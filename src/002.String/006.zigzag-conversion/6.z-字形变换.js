bef671b2d9bf48748a84767b86556fbce6f8424b
lizhi <15501251520@163.com>
2022-11-08 17:02:24 +0800

    feat: 6.zigzag-conversion flag 转换思路

--- src/002.String/006.zigzag-conversion/6.z-字形变换.js
+++ src/002.String/006.zigzag-conversion/6.z-字形变换.js
@@ -10,8 +10,35 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, numRows) {
+/*
+'PAYPALISHIRING' 3
 
+
+p   A   H   N     res[0] = 'PAHN'
+A P L S I I G     res[1] = 'APLSIIG'
+Y   I   R         res[2] = 'YIR'
+
+算法流程： 按顺序遍历字符串 s；
+1. res[i] += c： 把每个字符 c 填入对应行；
+2. i += flag： 更新当前字符 c 对应的行索引；
+3. flag = - flag： 在达到 ZZ 字形转折点时，执行反向.
+*/ 
+var convert = function(s, numRows) {
+  if (s.length <= 2) return s
+
+  let res = [], flag = -1, i = 0
+
+  for (const c of s) {
+    res[i] = (res[i] || '') + c
+    
+    if (i == 0 || i == numRows - 1) {
+      flag = -flag
+    }
+
+    i += flag
+  }
+
+  return res.join('')
 };
 // @lc code=end
 

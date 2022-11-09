f5794faf5a84ff0e6ec72416dd9944d8e5dac14f
lizhi <15501251520@163.com>
2022-11-09 11:32:26 +0800

    feat: 11.最大盛水 by js 双指针思想

--- src/001.Array/011.container-with-most-water/11.盛最多水的容器.js
+++ src/001.Array/011.container-with-most-water/11.盛最多水的容器.js
@@ -9,8 +9,26 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
+/*
+  s = min(h[i], h[j]) * (j - 1)
 
+  关键点: 每次移动较小指针端
+*/
+var maxArea = function(height) {
+  let i = 0, j = height.length - 1, maxs = 0
+
+  while (i <= j) {
+    let s = Math.min(height[i], height[j]) * (j - i)
+    maxs = Math.max(s, maxs)
+
+    if (height[i] < height[j]) {
+      i++
+    } else {
+      j--
+    }
+  }
+
+  return maxs
 };
 // @lc code=end
 
